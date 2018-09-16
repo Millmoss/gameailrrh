4e5ef19f45dc851f0d3e277e3b899ab9b347f890
Millmoss <omelliff@yahoo.com>
2018-09-16 17:46:55 -0400

    Updating files

---
+++ Game AI Lil Red Ridin Hood/Assets/Scripts/Character.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	public GameObject goal;
+	public GameObject head;
+	public float moveSpeed = 1;
+	public int mapBounds = 100;
+	public int randomSeed = 0;
+	private Rigidbody selfBody;
+	private float moveForce = 3;
+	private Vector3 movement;
+	private enum status { wandering, chasing, fleeing, pathFinding };
+	public int state = 0;
+	private float t = 0;
+
+	void Start ()
+	{
+		selfBody = gameObject.GetComponent<Rigidbody>();
+		Random.InitState(randomSeed);
+	}
+	
+	void Update ()
+	{
+		switch (state)
+		{
+			case ((int)status.wandering):
+				if (t == 0 || Vector3.Magnitude(transform.position - goal.transform.position) < 10)
+				{
+					float rad = Mathf.PI * Random.value * 2;
+					Vector3 newPos = transform.position + 30 * head.transform.forward + new Vector3(Mathf.Cos(rad) * 20, 0, Mathf.Sin(rad) * 20);
+					newPos = new Vector3(Mathf.Clamp(newPos.x, 5, mapBounds - 5), 10, Mathf.Clamp(newPos.z, 5, mapBounds - 5));
+					goal.transform.position = newPos;
+					LayerMask groundMask = 1 << 9;
+					Ray ray = new Ray(goal.transform.position, -goal.transform.up);
+					RaycastHit hit = new RaycastHit();
+					if (Physics.Raycast(ray, out hit, 1000f, groundMask))
+					{
+						goal.transform.position = hit.point;
+					}
+				}
+				t += Time.deltaTime;
+				if (t > 5)
+					t = 0;
+				move();
+				break;
+			default:
+				break;
+		}
+
+		movement = transform.position;
+	}
+
+	void FixedUpdate()
+	{
+		Quaternion look = Quaternion.LookRotation(goal.transform.position - transform.position);
+		head.transform.rotation = Quaternion.Slerp(head.transform.rotation, look, .3f);
+	}
+
+	void LateUpdate()
+	{
+		head.transform.position = transform.position + new Vector3(0, .3f, 0);
+	}
+
+	void move()
+	{
+		//move to goal
+		Vector3 direction = (goal.transform.position - transform.position).normalized;
+		selfBody.AddForce(direction * moveForce * moveSpeed);
+
+		//adjust movement for slope and pushback
+		float moveVariance = Vector3.Angle(transform.position - movement, direction);
+		if (moveVariance > 30)
+			selfBody.AddForce(direction + (direction - (transform.position - movement)) * moveForce * moveVariance / 15);
+	}
+}
---
+++ Game AI Lil Red Ridin Hood/Assets/Scripts/MeshGen.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MeshGen : MonoBehaviour
+{
+	GameObject coneMesh;
+	GameObject houseMesh;
+
+	void Start ()
+	{
+		/*coneMesh = new GameObject("Plane");
+		coneMesh.layer = 0;
+		coneMesh.AddComponent<MeshFilter>();
+		coneMesh.AddComponent<MeshRenderer>();
+
+		Vector3[] coneVertices = new Vector3[26];    //areamap in the first polyscale square, the inbetweens in the second
+		int[] coneTriangles = new int[48 * 3];
+		coneVertices[24] = new Vector3(0, 1, 0);
+		coneVertices[25] = new Vector3(0, 0, 0);
+		for (float i = 0; i < 24; i++)
+		{
+			float rad = i / 24 * 2 * Mathf.PI;
+			float x = Mathf.Cos(rad);
+			float z = Mathf.Sin(rad);
+			coneVertices[(int)i] = new Vector3(x * .5f, 0, z * .5f);
+		}
+
+		for (int i = 0; i < 23; i++)
+		{
+			coneTriangles[i * 6 + 0] = i + 1;
+			coneTriangles[i * 6 + 1] = i;
+			coneTriangles[i * 6 + 2] = 24;
+			coneTriangles[i * 6 + 3] = i;
+			coneTriangles[i * 6 + 4] = i + 1;
+			coneTriangles[i * 6 + 5] = 25;
+		}
+
+		coneTriangles[23 * 6 + 0] = 0;
+		coneTriangles[23 * 6 + 1] = 23;
+		coneTriangles[23 * 6 + 2] = 24;
+		coneTriangles[23 * 6 + 3] = 23;
+		coneTriangles[23 * 6 + 4] = 0;
+		coneTriangles[23 * 6 + 5] = 25;
+
+		coneMesh.GetComponent<MeshFilter>().mesh.vertices = coneVertices;
+		coneMesh.GetComponent<MeshFilter>().mesh.triangles = coneTriangles;
+		coneMesh.GetComponent<MeshFilter>().mesh.RecalculateNormals();
+
+		AssetDatabase.CreateAsset(coneMesh.GetComponent<MeshFilter>().mesh, "Assets/ConeMesh");
+		AssetDatabase.SaveAssets();*/
+
+		houseMesh = new GameObject("Plane");
+		houseMesh.layer = 0;
+		houseMesh.AddComponent<MeshFilter>();
+		houseMesh.AddComponent<MeshRenderer>();
+
+		Vector3[] houseVertices = new Vector3[10];    //areamap in the first polyscale square, the inbetweens in the second
+		int[] houseTriangles = new int[16 * 3];
+		houseVertices[0] = new Vector3(0, 0, 0);
+		houseVertices[1] = new Vector3(0, 0, 1);
+		houseVertices[2] = new Vector3(1, 0, 0);
+		houseVertices[3] = new Vector3(1, 0, 1);
+		houseVertices[4] = new Vector3(0, 1, 0);
+		houseVertices[5] = new Vector3(0, 1, 1);
+		houseVertices[6] = new Vector3(1, 1, 0);
+		houseVertices[7] = new Vector3(1, 1, 1);
+		houseVertices[8] = new Vector3(0, 1.5f, .5f);
+		houseVertices[9] = new Vector3(1, 1.5f, .5f);
+
+		houseTriangles[0] = 0;
+		houseTriangles[1] = 1;
+		houseTriangles[2] = 2;
+		houseTriangles[3] = 1;
+		houseTriangles[4] = 2;
+		houseTriangles[5] = 3;
+		houseTriangles[6] = 0;
+		houseTriangles[7] = 1;
+		houseTriangles[8] = 4;
+		houseTriangles[9] = ;
+		houseTriangles[10] = ;
+		houseTriangles[11] = ;
+
+		houseMesh.GetComponent<MeshFilter>().mesh.vertices = houseVertices;
+		houseMesh.GetComponent<MeshFilter>().mesh.triangles = houseTriangles;
+		houseMesh.GetComponent<MeshFilter>().mesh.RecalculateNormals();
+
+		AssetDatabase.CreateAsset(houseMesh.GetComponent<MeshFilter>().mesh, "Assets/houseMesh");
+		AssetDatabase.SaveAssets();
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
